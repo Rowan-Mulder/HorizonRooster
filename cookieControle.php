2b475f5d95473b801432146aed8ab1d19988e7b5
Rowan Mulder <62886548+Rowan-Mulder@users.noreply.github.com>
2021-02-06 12:36:27 +0100

    Fixed issue with secure cookies
    If I remember correctly, secure cookies wouldn't work on localhost and insecure cookies wouldn't work on https online. Added a condition which allows for local development again.

--- cookieControle.php
+++ cookieControle.php
@@ -16,13 +16,14 @@ Array
 
 if (!empty($_POST)) {
 	// Het domein waar de cookie wordt opgeslagen. Staat ook localhost toe voor testen
-	$domein = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;
+	$domein = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;// localhost of server
+	$secure = ($_SERVER['HTTP_HOST'] != 'localhost') ? true : false;// Bepaald of cookie voor http (localhost) of https (server) gemaakt wordt.
 	
 	$cookieOpslagDuurPost = (int)$_POST['cookieOpslagduur'];
 	$cookieOpslagDuur = (86400 * 365) * $cookieOpslagDuurPost;
 	
 	// Slaat de cookies op
-	setcookie('rooster', json_encode($_POST), time() + $cookieOpslagDuur, '/', $domein, true);
+	setcookie('rooster', json_encode($_POST), time() + $cookieOpslagDuur, '/', $domein, $secure);
 	$_COOKIE['rooster'] = json_encode($_POST);
 	
 	$roosterCookieAanwezig = true;
